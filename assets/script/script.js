aaf8b444d26eec738f156e4a73a557e24e78540b
TrumanStephenson <stephensontruman96@yahoo.com>
2023-07-17 21:13:16 -0700

    Added the generator, it is not currently functions properly. Will review next

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,39 @@
-const { createCanvas, loadImage, registerFont } = reuire('canvas');
+const { createCanvas, loadImage, registerFont } = require('canvas');
 const faker = require('faker');
 const path = require('path');
 
 // Set the font path
 registerFont(path.join(__dirname, 'fonts', 'Roboto-Regular.ttf'), 
 { family: 'Roboto'});
+
+// configure the card dimensions
+const cardWidth = 400;
+const cardHeight = 200;
+const textColor = '#000000';
+
+// generate random name
+const name = faker.name.findName();
+
+// Create a new canvas
+const canvas = createCanvas(cardWidth, cardHeight);
+const ctx = canvas.getContext('2d');
+
+// Set the background color
+ctx.fillStyle = textColor;
+ctx.fillRect(0, 0, cardWidth, cardHeight);
+
+// Set the text properties
+ctx.fillStyle = textColor;
+ctx.font = '24px Robot';
+
+// Draw the name
+ctx.fillText(name, 20, 50);
+
+// Save the image as a PNG file
+const imageBuffer = canvas.toBuffer('image/png');
+const fileName = '${name.toLowerCase().replace(' ' ,'-',)}_card.png;
+const filePath = path.join(__dirname, 'output', fileName);
+
+require('fs').writeFileSync(filePath, imageBuffer);
+
+console.log('Name card generated: ${fileName}');
